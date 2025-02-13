4b5e710e91b0c4639270e32d71b01c7d2a9f8417
Niroshan <niroshan9812@gmail.com>
2025-02-13 09:51:48 +0530

    Initial commit

---
+++ src/main/java/org/niroshan/eventconfarance/EventConfaranceApplication.java
@@ -0,0 +1,13 @@
+package org.niroshan.eventconfarance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventConfaranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventConfaranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/niroshan/eventconfarance/EventConfaranceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niroshan.eventconfarance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConfaranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
